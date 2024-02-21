9a9e0add666eb8e0d18516116c5355b9fe9250b5
lahaina <nattolecats@gmail.com>
2024-02-21 17:53:40 +0900

    audio: Simplify audio copy scripts

--- config/audio.mk
+++ config/audio.mk
@@ -1,51 +1,20 @@
 LOCAL_PATH := vendor/2by2/audio
-LOCAL_PATH_FWB := frameworks/base/data/sounds
-
-ALARM_FILES := Argon Carbon Helium Krypton Neon Oxygen Osmium Platinum Timer
-NOTIFICATION_FILES := Ariel Ceres Carme Elara Europa Iapetus Io Rhea Salacia Titan Tethys
-RINGTONE_FILES := Atria Callisto Dione Ganymede Luna Oberon Phobos Pyxis Sedna Titania Triton \
-	Umbriel
-EFFECT_FILES := Dock InCallNotification KeypressInvalid Lock LowBattery \
-    NFCFailure NFCInitiated NFCSuccess NFCTransferComplete NFCTransferInitiated \
-    Trusted Undock Unlock
 
 # Alarms
-
-PRODUCT_COPY_FILES += $(foreach fn,$(ALARM_FILES),\
-    $(LOCAL_PATH_FWB)/alarms/material/ogg/$(fn).ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/$(fn).ogg)
-
-$(foreach f, $(wildcard $(LOCAL_PATH)/alarms/*.m4a), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/$(notdir $f)))
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/alarms,$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms)
 
 # Ringtones
-
-PRODUCT_COPY_FILES += $(foreach fn,$(RINGTONE_FILES),\
-    $(LOCAL_PATH_FWB)/ringtones/material/ogg/$(fn).ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/$(fn).ogg)
-
-$(foreach f, $(wildcard $(LOCAL_PATH)/ringtones/*.m4a), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/$(notdir $f)))
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ringtones,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones)
 
 # Notifications
-
-PRODUCT_COPY_FILES += $(foreach fn,$(NOTIFICATION_FILES),\
-    $(LOCAL_PATH_FWB)/notifications/material/ogg/$(fn).ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/$(fn).ogg)
-
-$(foreach f, $(wildcard $(LOCAL_PATH)/notifications/*.m4a), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/$(notdir $f)))
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/notifications,$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications)
 
 # UI
-
-PRODUCT_COPY_FILES += $(foreach fn,$(EFFECT_FILES),\
-    $(LOCAL_PATH_FWB)/effects/ogg/$(fn).ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/$(fn).ogg)
-
-$(foreach f, $(wildcard $(LOCAL_PATH)/ui/*.ogg), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/$(notdir $f)))
-
-# 2by2-side
-$(foreach f, $(wildcard $(LOCAL_PATH)/2by2/*.mp3), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/$(notdir $f)))
-$(foreach f, $(wildcard $(LOCAL_PATH)/2by2/*.mp3), \
-    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/$(notdir $f)))
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ui,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
 
 # Default Sounds
 PRODUCT_PRODUCT_PROPERTIES += \
