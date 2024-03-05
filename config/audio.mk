9b29ad6a4e6027ae068d03543fe5622595e6f79f
lahaina <nattolecats@gmail.com>
2024-03-05 19:46:15 +0900

    audio: Update ringtone, notification sounds

--- config/audio.mk
+++ config/audio.mk
@@ -18,6 +18,6 @@ PRODUCT_COPY_FILES += \
 
 # Default Sounds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Prince_of_Darkness.mp3 \
+    ro.config.ringtone=Frail_Naysayer.mp3 \
     ro.config.alarm_alert=Alarm003301.m4a \
-    ro.config.notification_sound=Notice003205.m4a
+    ro.config.notification_sound=Notice003201.m4a
