e1d1b7eb4b41f20d3215bf74239255348a0720a3
lahaina <nattolecats@gmail.com>
2024-04-18 23:33:04 +0900

    overlay: Import CertifiedPropsOverlay

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -6,7 +6,12 @@ PRODUCT_PACKAGES += \
     2by2ExtSettingsStringOverlay \
     2by2LauncherOverlay \
     2by2LineageSetupWizardOverlay \
-    2by2SetupWizardOverlay
+    2by2SetupWizardOverlay \
+    CertifiedPropsOverlay
+
+PRODUCT_COPY_FILES += \
+    vendor/2by2/rro_overlays/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml \
+    vendor/2by2/rro_overlays/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml
 
 # Standard Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
