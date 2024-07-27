a2b8cc5a74243c8cd679806fc4126363bec2cf29
lahaina <nattolecats@gmail.com>
2024-07-28 01:26:15 +0900

    common: Enable blur effect by default

--- config/common.mk
+++ config/common.mk
@@ -40,6 +40,15 @@ $(call inherit-product, vendor/2by2/prebuilts/fonts_sgr_sgm/fonts.mk)
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     $(strip $(shell cat vendor/2by2/config/artifact_path_requirement_allowed_list.txt))
 
+# Conditionally enable blur
+TARGET_USES_BLUR ?= true
+
+ifeq ($(TARGET_USES_BLUR), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
+
 # Keys
 -include vendor/2by2-example-keys/keys.mk
 
