0e7f2dfa7d2dba82c2f360088b1ad8f1119c0568
kailua <kailua@project2by2.jp>
2025-04-05 00:06:59 +0900

    build: tasks: Automatically copy json

--- build/tasks/otapackage.mk
+++ build/tasks/otapackage.mk
@@ -6,6 +6,8 @@ otapackage:
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(CUSTOM_TARGET_PACKAGE) | cut -d ' ' -f1 > $(CUSTOM_TARGET_PACKAGE).sha256sum
 	$(hide) source ./vendor/2by2/tools/generate_json_build_info.sh $(CUSTOM_TARGET_PACKAGE)
+	$(hide) mkdir -p vendor/2by2-ota/$(CUSTOM_BUILD)/
+	$(hide) cp $(PRODUCT_OUT)/$(CUSTOM_BUILD).json vendor/2by2-ota/$(CUSTOM_BUILD)/$(CUSTOM_BUILD).json
 	@echo -e ""
 	@echo -e "2by2 Project OTA package build completed."
 	@echo -e ""
