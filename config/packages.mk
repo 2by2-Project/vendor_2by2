6e2c3a1148b130449de376e10147e0624f266da1
kailua <kailua@project2by2.jp>
2025-05-15 18:48:08 +0900

    packages: Import face unlock

--- config/packages.mk
+++ config/packages.mk
@@ -123,5 +123,17 @@ PRODUCT_BOOT_JARS += \
     tcmiface \
     telephony-ext
 
+# Face Unlock
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Extra apps
 $(call inherit-product-if-exists, vendor/2by2-prebuilt-apps/apps.mk)
